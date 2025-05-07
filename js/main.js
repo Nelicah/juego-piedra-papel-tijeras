20927f2f1c1fc5a37eec1a3b6c47d1edc0e81b71
Nelicah <candezmr2@gmail.com>
2025-05-07 09:22:50 +0200

    comprobando que funciona el numero random

--- js/main.js
+++ js/main.js
@@ -10,3 +10,13 @@ const buttonPlay = document.querySelector('.js_buttonPlay');
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
   }
+
+if (getRandomNumber <= 3) {
+  console.log('La computadora ha sacado "Piedra"');
+} else if (getRandomNumber >= 7) {
+  console.log('La computadora ha sacado "Papel"');
+} else {
+  console.log ('La computadora ha sacado "Tijera"');
+};
+console.log(getRandomNumber(9));
+console.log(getRandomNumber(9));
